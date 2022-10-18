ad9183ec42624029b40aff7c2a117b645c538a60
aniekanmark55 <113603728+aniekanmark55@users.noreply.github.com>
2022-10-18 19:34:05 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,14 +4,8 @@
  * @n: input
  * Return: Always 0 (Success)
  */
-void print_times_table(int n)
+void 'print_times_table'(int n)
 {
-    print_times_table(3);
-    _putchar('\n');
-    print_times_table(5);
-    _putchar('\n');
-    print_times_table(98);
-    _putchar('\n');
-    print_times_table(12);  
-    return (0);
+
+	return (0);
 }
