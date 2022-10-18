886b630ee12063e4268ba71537c30b43882ad4e6
“aniekanmark55” <aniekanmark55@gmail.com>
2022-10-18 10:44:18 -0700

    Task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+nclude <stdio.h>
+
+#include <stdlib.h>
+
+#include "holberton.h"
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+
+
+
+		return (0);
+
+}
