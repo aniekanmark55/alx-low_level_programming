89ea0f45df94c891763007ae05ee6cbf4a87f57e
aniekanmark55 <113603728+aniekanmark55@users.noreply.github.com>
2022-10-18 19:03:37 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,21 +1,11 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
- *
- *  * print_times_table - Entry point
- *
- *   * @n: input
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ * print_times_table - Entry point
+ * @n: input
+ * Return: Always 0 (Success)
+ */
 void print_times_table(int n)
-
 {
 
-
-
-		return (0);
-
+	return (0);
 }
