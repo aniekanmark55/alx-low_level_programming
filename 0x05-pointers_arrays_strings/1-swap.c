bfc22167c2e4d9debf4832982d5240eb58c89095
“aniekanmark55” <aniekanmark55@gmail.com>
2022-10-24 06:10:39 -0700

    Task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* swap_int - swaping 2 integer using a pointer
+* @a:integer
+* @b:integer
+* Return:void
+*/
+void swap_int(int *a, int *b)
+{
+	int x;
+
+	p = *a;
+	*a = *b;
+	*b = p;
+	return;
+
+}
