54fd49ac77663a9efad2147f0bfd04ca3614d9f3
“aniekanmark55” <aniekanmark55@gmail.com>
2023-01-16 13:21:59 -0800

    Task 4

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/aniekanmark55/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libmask.so
-export LD_PRELOAD=/tmp/libmask.so
+wget -P /tmp https://github.com/aniekanmark55/alx-low_level_programming/raw/master/0x18-dynamic_libraries/iwin.so
+export LD_PRELOAD=/tmp/iwin.so
